49450823503f49e445da7823216c8e90e4a5e5b6
romaniukua <romaniukvadym1511@gmail.com>
2019-07-01 11:01:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from './components/header/Header';
+import Banner from './components/banner/Banner';
+import Cards from './components/cards/Cards';
+import PostForm from './components/postForm/PostForm';
+import Footer from './components/footer/Footer';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Header/>
+          <main className='main'>
+            <Banner/>
+            <Route exact path='/' component ={Cards} />
+            <Route path='/contact' component ={PostForm}/>
+          </main>
+          <Footer/>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
