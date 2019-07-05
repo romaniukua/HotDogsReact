7f3e19e2cc7b374dd2821241b2c72d514d9ea837
romaniukua <romaniukvadym1511@gmail.com>
2019-07-05 14:57:03 +0300

    Updates

---
+++ precache-manifest.ea2498adf8782090cb47bc0cec08c1f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f15b7ea74ef1aed902f533c52051184",
+    "url": "/hotdogs-react/index.html"
+  },
+  {
+    "revision": "8a7da04218aac2cd8572",
+    "url": "/hotdogs-react/static/css/main.3b9bc8a8.chunk.css"
+  },
+  {
+    "revision": "580eaf52e2c09ef5c33a",
+    "url": "/hotdogs-react/static/js/2.662c746a.chunk.js"
+  },
+  {
+    "revision": "8a7da04218aac2cd8572",
+    "url": "/hotdogs-react/static/js/main.38468a5c.chunk.js"
+  },
+  {
+    "revision": "b019f22826f5b4ea722c",
+    "url": "/hotdogs-react/static/js/runtime~main.44b0def9.js"
+  },
+  {
+    "revision": "bbae08443b1e86703edf5f861ead4c8a",
+    "url": "/hotdogs-react/static/media/head_banner.bbae0844.png"
+  },
+  {
+    "revision": "4acd556cb8054d6411ce402902c51e12",
+    "url": "/hotdogs-react/static/media/hotdog.4acd556c.svg"
+  }
+]);
